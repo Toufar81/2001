77c3028546b28f098dc6d68d460df76ae1147362
Toufar Libor <toumou88@gmail.com>
2025-03-04 20:16:05 +0100

    Přidan login - name

--- name.py
+++ name.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        first_name = request.form.get("first_name")
+        secund_name = request.form.get("second_name")
+        return render_template("index.html",first_name=first_name,secund_name=secund_name)
+    else:
+        return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
