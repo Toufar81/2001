2f4054e20ef8d6badd35508a6ab9c3c8b7f3e913
Toufar Libor <toumou88@gmail.com>
2025-03-04 23:28:18 +0100

    Přidana tlačitka START a RULES na stranku welcom_star.html + animace v style1.css

--- name.py
+++ name.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+name = []
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
         first_name = request.form.get("first_name")
         secund_name = request.form.get("second_name")
-        return render_template("index.html",first_name=first_name,secund_name=secund_name)
+        name.append(first_name)
+        name.append(secund_name)
+        return render_template("welcom_start.html",first_name=first_name,secund_name=secund_name)
     else:
         return render_template("index.html")
 
+@app.route('/start',methods=['GET','POST'])
+def start():
+
+    return "Tlacitko bylo stisknuto"
 if __name__ == '__main__':
     app.run(debug=True)
